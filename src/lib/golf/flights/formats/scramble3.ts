5e6ceb31db9413200d811eb8b93abb034cc7bb80
didier lozet <didierlozet@Imac-Didier.local>
2026-04-10 14:22:07 +0200

    fix scramble3 bestTeams type

--- src/lib/golf/flights/formats/scramble3.ts
+++ src/lib/golf/flights/formats/scramble3.ts
@@ -1,8 +1,7 @@
 import { buildFlights } from "@/lib/utils/buildFlights"
 import { shuffle } from "@/lib/utils/shuffle"
 
-export function scramble3Flights(players) {
-
+export function scramble3Flights(players: any[]) {
   const shuffled = shuffle(players)
 
   return buildFlights(shuffled, 3)
