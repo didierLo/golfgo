7878887b3ad5a604e624445feda8d5f4575122f8
didier lozet <didierlozet@Imac-Didier.local>
2026-04-11 15:11:15 +0200

    feat: favicon GolfGo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "GolfGo",
   description: "Organise tes parties de golf",
+  icons: {
+    icon: "/favicon.png",
+    apple: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
