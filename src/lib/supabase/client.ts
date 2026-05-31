8fa4fa6ab9e558257a60b2866b490bab86742bde
didier lozet <didierlozet@Imac-Didier.local>
2026-05-31 19:02:36 +0200

    fix: supabase clients

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,10 +1,16 @@
 // src/lib/supabase/client.ts
+// src/lib/supabase/client.ts
 'use client';
 
 import { createBrowserClient } from '@supabase/ssr'
 
-export const createClient = () =>
-  createBrowserClient(
+let client: ReturnType<typeof createBrowserClient> | null = null
+
+export const createClient = () => {
+  if (client) return client
+  client = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  )
+  return client
+}
